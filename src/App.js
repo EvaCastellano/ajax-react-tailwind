939215458810961c20f34452de559964ec8b9aad
EvaCastellano <evacastellano60@gmail.com>
2024-02-27 08:49:58 +0100

    26-02-2024

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Tabla from './Tabla'
+import API from './api'
 
 function App() {
+  const [categorias, setCategorias] = useState([])
+  const [platos, setPlatos] = useState([])
+  const [restaurantes, setRestaurantes] = useState([])
+
+  useEffect(() => {
+    fetch(API.CATEGORIES_URL)
+      .then((res) => res.json())
+      .then((categorias) => setCategorias(categorias))
+
+    fetch(API.DISHES_URL)
+      .then((res) => res.json())
+      .then((platos) => setPlatos(platos))
+
+    fetch(API.RESTAURANTS_URL)
+      .then((res) => res.json())
+      .then((restaurantes) => setRestaurantes(restaurantes))
+  }, [])
+
+  const datosCaragados = Boolean(
+    restaurantes.length && platos.length && categorias.length
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline">Lista de Restaurantes</h1>
+      {datosCaragados && (
+        <Tabla
+          restaurantes={restaurantes}
+          platos={platos}
+          categorias={categorias}
+        />
+      )}
+      {!datosCaragados && <p>Cargando datos...</p>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
